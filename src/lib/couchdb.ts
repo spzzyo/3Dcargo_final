2615391e1a1e43b585f1bb48a3fe4549e990f6ca
spzzyo <yoshita.singh@spit.ac.in>
2025-05-20 17:05:41 +0530

    fixed db views

--- src/lib/couchdb.ts
+++ src/lib/couchdb.ts
@@ -4,6 +4,9 @@ import nano from 'nano';
 const couch = nano('http://admin:admin123@10.119.11.41:5984');
 const dbName = 'cargodb';
 
+// const couch = nano('http://admin:admin@localhost:5984');
+// const dbName = 'iitbdb';
+
 export const getCouchDb = async () => {
   const dbList = await couch.db.list();
   if (!dbList.includes(dbName)) {
